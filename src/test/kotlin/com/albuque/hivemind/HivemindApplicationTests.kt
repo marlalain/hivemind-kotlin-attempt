1944ddbcd8f34cd42582ecdb6d6c7d40254296b0
Marla Albuquerque <marla@albuque.com>
2024-08-29 22:57:38 -0300

    chore(initializr): init

---
+++ src/test/kotlin/com/albuque/hivemind/HivemindApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.albuque.hivemind
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HivemindApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
